6101c4f7958c246fdd067377f469680d9accf209
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-09-07 13:29:26 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     i = len(sys.argv) - 1
 
     if i == 0:
-        print("{} arguments.")
+        print("{} arguments.".format(i))
     elif i == 1:
-        print("{} argument:")
+        print("{} argument:".format(i))
     else:
-        print("{} arguments:")
+        print("{} arguments:".format(i))
 
     if i >= 1:
         i = 0
