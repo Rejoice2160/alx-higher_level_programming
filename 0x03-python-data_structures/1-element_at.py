f0dcc83b3f60017f07f0e1aa79150c33ac4cac4a
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-09-10 07:22:21 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # 1-element_at.py
+
+
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
@@ -9,4 +11,4 @@ def element_at(my_list, idx):
     if idx > length - 1:
         return (None)
 
-    return(my_list)
+    return(my_list[idx])
