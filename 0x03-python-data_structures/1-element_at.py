bef4dbef71e87256874ab6559d8383cf06b0fc6d
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-09-11 01:37:02 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    # Check if idx is negative
     if idx < 0:
-        return None
+        return None  
 
-    # Initialize a variable to keep track of the current index
-    current_idx = 0
+    length = len(my_list)
 
-    # Iterate through the list
-    for element in my_list:
-        # Check if the current index matches the desired index (idx)
-        if current_idx == idx:
-            return element
-        current_idx += 1
+    if idx > length - 1:
+        return None  
 
-    # If we reach this point, idx is out of range
-    return None
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-idx = 2
-result = element_at(my_list, idx)
-print(result)  # This will print 3
-
-idx = 10
-result = element_at(my_list, idx)
-print(result)  # This will print None since idx is out of range
+    return my_list[idx]
