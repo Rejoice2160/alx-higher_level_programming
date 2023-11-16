7ed37077fc2452df3767e1e9f09d16e520c07821
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-11-16 15:23:38 -0800

    updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 -- Prints the full description of the table first_table in my MySQL server.
 SHOW CREATE TABLE `first_table`;
-=======
--- Prints the full description of the table first_table from the database
--- Query to print full description of a table
-SHOW CREATE TABLE first_table;
->>>>>>> 0996663cd03a9b36a03118c84663faab64c4ed47
