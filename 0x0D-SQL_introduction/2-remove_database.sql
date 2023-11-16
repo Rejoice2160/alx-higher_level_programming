bade7bd7dc49934a52b870c1df383b6f154a4da6
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-11-16 15:25:29 -0800

    updated

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 -- Deletes the database hbtn_0c_0 from my MySQL server.
 DROP DATABASE IF EXISTS `hbtn_0c_0`;
-=======
--- Deletes a database
--- Query to delete the database hbtn_0c_0 in MySQL server
-DROP DATABASE IF EXISTS hbtn_0c_0;
->>>>>>> 0996663cd03a9b36a03118c84663faab64c4ed47
+
