080e829d32b8827fd2c3e7da5c4aede9654c707a
Rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-11-16 14:53:22 -0800

    updated

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 -- Lists the number of records with the same score in the table second_table in my MySQL server.
 -- Records are ordered by descending count.
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
 ORDER BY `number` DESC;
-=======
--- Script that lists the number of records with the same score
--- Query to lists the number of records with the same score
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score
-ORDER BY score DESC;
->>>>>>> 0996663cd03a9b36a03118c84663faab64c4ed47
